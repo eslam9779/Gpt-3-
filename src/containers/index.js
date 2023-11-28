815824de0edf504da72d22ca51233db5b9c1f388
eslam9779 <eslamabdo9779@gmail.com>
2023-11-29 00:57:35 +0200

    Add Nav bar

---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+export {default as Blog} from './blog/Blog';
+export {default as Feature} from './feature/Feature';
+export {default as Footer} from './footer/Footer';
+export {default as Header} from './header/Header';
+export {default as Posibility} from './posibility/Posibility';
+export {default as Whatsgpt} from './whatsGpt/Whatsgpt';
+
---
+++ src/containers/whatsGpt/Whatsgpt.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatsgpt.css'
+
+const Whatsgpt = () => {
+  return (
+    <div>
+      Whatsgpt
+    </div>
+  )
+}
+
+export default Whatsgpt;
